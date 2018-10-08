5963256009982dcd0af25301d020affede96c1a5
burin <21989631+burinm@users.noreply.github.com>
2018-10-08 12:40:03 -0600

    Added interrupt pin to accelerator

--- api/i2c.h
+++ api/i2c.h
@@ -6,6 +6,7 @@
 #define I2C0_SDA_LOCATION    16 //PC11 - EXP header #16
 #define I2C0_SCL_LOCATION    14 //PC10 - EXP header #15
 #define ACCELEROMETER_CS     PF5 //TDB
+#define ACCELEROMETER_INT1   PF4 //TDB
 
 void I2C0_init();
 
